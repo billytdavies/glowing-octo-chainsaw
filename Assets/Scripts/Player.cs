a4af97e2c903fda1d4f465922715864c72d4fd04
billy.davies <billy@onedavies.com>
2018-02-02 16:17:40 +0000

    add fix

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		move();
+	}
+	void move(){
+		float dir = Input.GetAxis("Horizontal");
+		Vector3 pos = transform.position;
+		pos.x += dir*50;
+		transform.position = pos;
+		print(pos);
+	}
+}
